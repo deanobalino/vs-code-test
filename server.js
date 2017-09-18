74f6dfc7524ea643b0c01f3f6413cc9e940366e2
deanobalino <deanbryen@hotmail.com>
2017-09-18 14:33:20 +0100

    fixed it?

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps! Dean Here! Trying again!!</h1>');
+    res.end('<h1>Hello Code and Azure Web Apps! Dean Here! Trying again!! I think I fixed it!!</h1>');
 }).listen(process.env.PORT);
